3971d3adaffce01d9fa0ee2db1918e6d9f237cbf
JekaH1 <ceyhun100203@gmail.com>
2023-07-09 00:56:33 +0400

    ap done

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@
 // const nameSort = document.querySelector('.name-order');
 // const surnameSort = document.querySelector('.surname-order');
 // const salarySort = document.querySelector('.salary-order');
-// const removeFiltersBtn = document.querySelector('.btn-danger');
 // let nameCounter = 0;
 // let surnameCounter = 0;
 // let ageCounter = 0;
@@ -35,7 +34,13 @@ const pCatFilter = document.querySelector('.pCat-filter');
 const pMinPriceFilter = document.querySelector('.p-min-price-filter');
 const pMaxPriceFilter = document.querySelector('.p-max-price-filter');
 
+const pNameFilterMain = document.querySelector('.pName-filter-main');
+const pCatFilterMain = document.querySelector('.pCat-filter-main');
+const pMinPriceFilterMain = document.querySelector('.p-min-price-filter-main');
+const pMaxPriceFilterMain = document.querySelector('.p-max-price-filter-main');
+
 const addBtn = document.querySelector('.add-btn');
+const removeFiltersBtn = document.querySelectorAll('.remove-filters');
 
 
 let items = [];
@@ -64,6 +69,7 @@ function addToTable() {
     pPriceInput.value = '';
     pImageinput.value = '';
     showData(filterTheDataForAp());
+    showDataForSelling(filterTheDataForMain());
 
 }
 
@@ -81,9 +87,23 @@ function eventListeners() {
     pMaxPriceFilter.addEventListener('input', () => {
         showData(filterTheDataForAp())
     });
+    pNameFilterMain.addEventListener('input', () => {
+        showDataForSelling(filterTheDataForMain())
+    });
+    pCatFilterMain.addEventListener('input', () => {
+        showDataForSelling(filterTheDataForMain())
+    });
+    pMinPriceFilterMain.addEventListener('input', () => {
+        showDataForSelling(filterTheDataForMain())
+    });
+    pMaxPriceFilterMain.addEventListener('input', () => {
+        showDataForSelling(filterTheDataForMain())
+    });
+    removeFiltersBtn.forEach(element => {
+        element.addEventListener('click', removeFilters);
+    });
+
 }
-
-
 
 function getArrFromLocalStorage(key) {
     const localStoregaItemArr = localStorage.getItem(`${key}`);
@@ -122,13 +142,60 @@ function filterTheDataForAp() {
 //////////////////////////////////////////////////////
 
 const itemCounter = document.querySelector('.item-counter');
+const mainProductsWrapperCard = document.querySelector('.main-products-wrapper-cards');
 
+
+function filterTheDataForMain() {
+    const nameFilterValue = pNameFilterMain.value.toLowerCase();
+    const catFilterValue = pCatFilterMain.value.toLowerCase();
+    const minPriceFilterValue = pMinPriceFilterMain.value;
+    const maxPriceFilterValue = pMaxPriceFilterMain.value;
+    console.log(nameFilterValue, catFilterValue, minPriceFilterValue, maxPriceFilterValue);
+
+    const localStoregaItemArr = getArrFromLocalStorage('products');
+
+    const filteredData = localStoregaItemArr.filter(element => {
+        const name = element.name.toLowerCase();
+        const category = element.category.toLowerCase();
+        const price = Number(element.price);
+
+        const nameMatch = name.includes(nameFilterValue);
+        const categoryMatch = category.includes(catFilterValue);
+        const salaryMatch =
+            (minPriceFilterValue === '' || price >= Number(minPriceFilterValue)) &&
+            (maxPriceFilterValue === '' || price <= Number(maxPriceFilterValue));
+
+        return nameMatch && categoryMatch && salaryMatch;
+
+    });
+
+    return filteredData;
+}
 
 function showDataForSelling(callback) {
     const filteredData = callback;
-    tableInner.innerHTML = '';
+    itemCounter.innerHTML = filteredData.length + ' ' + 'items found';
+    mainProductsWrapperCard.innerHTML = '';
     filteredData.forEach((element) => {
-        tableInner.innerHTML += ``;
+        mainProductsWrapperCard.innerHTML += `<div class="col-3 mt-5 offer-card-col ">
+        <div class="offer-card-wrapper ">
+            <div class="offer-card-image-wrapper">
+                <img class="offer-card-image" src="./assets/images/${element.Image}.jpg"
+                    alt="a-man-with-Tshirt">
+            </div>
+            <div class="offer-card-inner-wrapper">
+                <h6 class="offer-card-brand-name pt-4"> ${element.name}</h6>
+                <p>${element.category}</p>
+            </div>
+            <div class="offer-card-price">
+                <span>$${element.price}</span><span class="px-2 secondary-span ">With Fee
+                    included</span>
+
+                <i class="fa-solid fa-cart-shopping add-to-card" ><input type="hidden" value="${element.id}"></i>
+                
+            </div>
+        </div>
+    </div>`;
     });
 }
 ///////////////////////////////////////////////////////
@@ -138,28 +205,111 @@ function showData(callback) {
     filteredData.forEach((element) => {
         tableInner.innerHTML += ` <tr>
         <th scope="row">${element.id}</th>
-        <td> <input type="text" value="${element.name}" class="nonbordered-input" readonly></td>
-        <td><input type="text" value="${element.category}" class="nonbordered-input" readonly>
+        <td> <input type="text" value="${element.name}" class="nonbordered-input crud-name" readonly></td>
+        <td><input type="text" value="${element.category}" class="nonbordered-input crud-cat" readonly>
         </td>
-        <td><input type="text" value="${element.price}" class="nonbordered-input" readonly>
+        <td><input type="text" value="${element.price}" class="nonbordered-input crud-price" readonly>
         </td>
         <td>
             <div class="ap-item-photo">
                 <img src="./assets/images/${element.Image + ".jpg"}" alt="">
             </div>
             <div class="ap-item-image-string">
-                <input type="hidden" value="${element.id}" class="nonbordered-input"
+                <input type="hidden" value="${element.Image + ".jpg"}" class="nonbordered-input crud-image"
                     readonly>
             </div>
         </td>
-        <td><button class="btn btn-danger" value="${element.id}">Delete</button></td>
+        <td><button class="btn btn-danger delete-btn-ap" value="${element.id}">Delete</button>
+        <button class="btn btn-success save-btn-ap" value="${element.id}">Save</button></td>
     </tr>`;
     });
 }
+const targettedItems = [];
 
+function update() {
+    document.addEventListener('click', function (e) {
+        const saveBtn = document.querySelectorAll('.btn-success');
+        const localStoregaItemArr = getArrFromLocalStorage('products');
+        if (e.target.getAttribute('readonly') !== null) {
+            e.target.removeAttribute('readonly');
+            targettedItems.push(e.target);
+            console.log(targettedItems);
+            saveBtn.forEach(saveBtn => {
+                saveBtn.addEventListener('click', function () {
+                    localStoregaItemArr.forEach(item => {
+                        if (item.id === Number(saveBtn.value)) {
+                            targettedItems.forEach(tgItem => {
+                                if (tgItem.classList.contains('crud-name')) {
+                                    item.name = tgItem.value;
+                                    localStorage.setItem('products', JSON.stringify(localStoregaItemArr));
+                                    console.log(item + "ad");
+                                    e.target.setAttribute('readonly', '');
+                                }
+                                if (tgItem.classList.contains('crud-cat')) {
+                                    item.category = tgItem.value;
+                                    localStorage.setItem('products', JSON.stringify(localStoregaItemArr));
+                                    console.log(item + "categoru");
+                                    e.target.setAttribute('readonly', '');
+                                }
+                                if (tgItem.classList.contains('crud-price')) {
+                                    item.price = tgItem.value;
+                                    localStorage.setItem('products', JSON.stringify(localStoregaItemArr));
+                                    console.log(item + "qiymet");
+                                    e.target.setAttribute('readonly', '');
+                                }
+                            })
+                        }
+                    });
+                });
+            });
+        }
+    });
 
+}
+
+function remove() {
+    document.addEventListener('click', function (params) {
+        const localStoregaItemArr = getArrFromLocalStorage('products');
+        const removeBtns = document.querySelectorAll('.delete-btn-ap');
+
+        removeBtns.forEach(removeBtn => {
+            removeBtn.addEventListener('click', () => {
+                localStoregaItemArr.forEach(lcItem => {
+                    console.log(lcItem);
+                    if (lcItem.id === Number(removeBtn.value)) {
+                        localStoregaItemArr.pop(lcItem);
+                        localStorage.setItem('products', JSON.stringify(localStoregaItemArr));
+                        console.log("Salal");
+                        window.location.reload();
+                    }
+                })
+            })
+
+        })
+    })
+}
+
+update();
+remove();
 eventListeners();
 showData(filterTheDataForAp());
+showDataForSelling(filterTheDataForMain());
+
+function removeFilters() {
+
+    pNameFilter.value = '';
+    pCatFilter.value = '';
+    pMinPriceFilter.value = '';
+    pMaxPriceFilter.value = '';
+    pNameFilterMain.value = '';
+    pCatFilterMain.value = '';
+    pMinPriceFilterMain.value = '';
+    pMaxPriceFilterMain.value = '';
+
+    showData(filterTheDataForAp());
+    showDataForSelling(filterTheDataForMain());
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -328,7 +478,7 @@ showData(filterTheDataForAp());
 //     nameSort.addEventListener('click', sortByName);
 //     surnameSort.addEventListener('click', sortBySurname);
 //     salarySort.addEventListener('click', sortBySalary);
-//     removeFiltersBtn.addEventListener('click', removeFilters)
+//
 
 // }
 
